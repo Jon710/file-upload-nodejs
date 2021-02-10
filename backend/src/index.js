5514705ec84382a521222b57bec011015d0966f7
João Luis Moraes <joaoluismoraes@Joaos-MacBook-Pro-2.local>
2021-02-10 19:57:59 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,39 @@
+const http = require("http");
+const socketIo = require("socket.io");
+const { logger } = require("./util.js");
+const Routes = require("./routes");
+
+const PORT = 3000;
+
+const handler = function (request, response) {
+  const defaultRoute = async (request, response) => response.end("Hello!");
+
+  const routes = new Routes(io);
+  const chosen = routes[request.method.toLowerCase()] || defaultRoute;
+
+  return chosen.apply(routes, [request, response]);
+};
+
+const server = http.createServer(handler);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    credentials: false,
+  },
+});
+
+io.on("connection", (socket) => logger.info("Alguém conectou! " + socket.id));
+
+// usamos emit para enviar uma mensagem para toda a rede, e emitir em um canal
+// ou evento especifico
+// const interval = setInterval(() => {
+//   io.emit("file-uploaded", 5e6);
+// }, 250);
+
+const startServer = () => {
+  const { address, port } = server.address();
+  logger.info(`app running at http://${address}:${port}`);
+};
+
+server.listen(PORT, startServer);
